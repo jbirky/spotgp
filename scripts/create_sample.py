d55441a237f6e586b267e2db4045e2f03214a991
jbirky <jbirky@uw.edu>
2024-01-02 12:33:45 -0800

    add options

---
+++ scripts/create_sample.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(100)
+
+# nsample = int(1e4)
+
+# tsample = {}
+# tsample["sim_id"] = np.arange(0, nsample)
+# tsample["peq"] = np.random.uniform(0.1, 25, nsample)
+# tsample["kappa"] = np.random.uniform(-1, 1, nsample)
+# tsample["inc"] = np.arccos(np.random.uniform(0, 1, nsample))
+# tsample["nspot"] = np.random.randint(10, 1000, nsample)
+# tsample["lspot"] = np.random.uniform(0, 50, nsample)
+# tsample["tau"] = np.random.uniform(0, 50, nsample)
+# tsample["alpha_max"] = np.exp(np.random.uniform(-3, -1, nsample))
+
+# train_sample = pd.DataFrame(data=tsample)
+# train_sample.to_csv("../files/training_parameters.csv", index=False)
+
+# =================================================
+
+# nsample = 500
+
+# tsample = {}
+# tsample["sim_id"] = np.arange(0, nsample)
+# tsample["peq"] = np.random.uniform(0.1, 25, nsample)
+# tsample["kappa"] = np.zeros(nsample)
+# tsample["inc"] = np.ones(nsample) * np.pi/2
+# tsample["nspot"] = np.random.randint(10, 1000, nsample)
+# tsample["lspot"] = np.random.uniform(0, 50, nsample)
+# tsample["tau"] = np.random.uniform(0, 50, nsample)
+# tsample["alpha_max"] = np.exp(np.random.uniform(-3, -1, nsample))
+
+# train_sample = pd.DataFrame(data=tsample)
+# train_sample.to_csv("../files/training_parameters2.csv", index=False)
+
+# =================================================
+
+# nsample = int(1e3)
+
+# tsample = {}
+# tsample["sim_id"] = np.arange(0, nsample)
+# tsample["peq"] = np.random.uniform(0.1, 25, nsample)
+# tsample["kappa"] = np.random.uniform(-1, 1, nsample)
+# tsample["inc"] = np.arccos(np.random.uniform(0, 1, nsample))
+# tsample["nspot"] = np.random.randint(10, 1000, nsample)
+# tsample["lspot"] = np.random.uniform(0, 50, nsample)
+# tsample["tau"] = np.random.uniform(0, 50, nsample)
+# tsample["alpha_max"] = 10**(np.random.uniform(-3, -1, nsample))
+
+# train_sample = pd.DataFrame(data=tsample)
+# train_sample.to_csv("../files/training_parameters3.csv", index=False)
+
+# =================================================
+
+nsample = int(1e3)
+
+tsample = {}
+tsample["sim_id"] = np.arange(0, nsample)
+tsample["peq"] = np.random.uniform(0.1, 25, nsample)
+tsample["kappa"] = np.random.uniform(-1, 1, nsample)
+tsample["inc"] = np.arccos(np.random.uniform(0, 1, nsample))
+tsample["nspot"] = np.random.randint(10, 100, nsample)
+tsample["tau"] = np.random.uniform(0, 50, nsample)
+tsample["alpha_max"] = 10**(np.random.uniform(-3, -1, nsample))
+
+train_sample = pd.DataFrame(data=tsample)
+train_sample.to_csv("../files/training_parameters4.csv", index=False)
--- scripts/train_coeff.py
+++ scripts/train_coeff.py
@@ -7,20 +7,31 @@ import sys
 sys.path.append("../src")
 import kernel_fourier as kf
 
+os.nice(10)
 
 # ================================================
 # Configuration 
 
-ncore = 48
-os.nice(10)
+ncore = 64
+trial = "3"
+save_outputs = False
+save_plots = False
 
-df = pd.read_csv("../files/training_parameters.csv")
+df = pd.read_csv(f"../files/training_parameters{trial}.csv")
 df = df[['peq', 'kappa', 'inc', 'nspot', 'lspot', 'tau', 'alpha_max']]
 train_dict = df.to_dict(orient='records')
 
+save_file = f"../files/train_parameters_fit_coefficients{trial}.csv"
+data_dir = f"../results/data{trial}/"
+plot_dir = f"../results/plots{trial}/"
+
+for path in [plot_dir, data_dir]:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 # ================================================
 
-def fit_kernel_parameters(ii, results_dir="../results/"):
+def fit_kernel_parameters(ii):
     
     hyperparam = train_dict[ii]
     train_kernel = kf.TrainKernel(hyperparam, tsim=1000, tsamp=0.1, nsim=5e2, tcut=200, fit_ft=False)
@@ -29,27 +40,37 @@ def fit_kernel_parameters(ii, results_dir="../results/"):
              "a11": 1.0, "a21": 0.1, "b11": 0, "b21": 0, "c11": hyperparam["tau"], "c21": hyperparam["tau"], 
              "a12": 0, "a22": 0, "c12": hyperparam["tau"], "c22": hyperparam["lspot"]}
 
+    # fit model coefficients to kernel
     plist, pdict = train_kernel.fit_model(pinit)
     ypred, power_pred = train_kernel.predict(pdict)
-    
-    # Save figure
-    str_index = f"{ii}".rjust(5, "0")
-    fig = train_kernel.plot_kernel_fit(ypred, text_blocks=[kf.format_title(par) for par in plist])
-    fig.savefig(os.path.join(results_dir, f"plots/sim_{str_index}.png"), bbox_inches="tight")
-    
-    # Save training data
-    np.savez(os.path.join(results_dir, f"data/sim_{str_index}.png"), xdata=train_kernel.ydata, ydata=train_kernel.ydata,
-         freq=train_kernel.freq, power=train_kernel.power,
-         ypred=ypred, power_pred=power_pred)
-    
-    return {**{"sim_id": ii}, **hyperparam, **pdict}
 
+    # compute error
+    ymse = np.mean((ypred - train_kernel.ydata)**2)
+    pmse = np.mean((power_pred - train_kernel.power)**2)
+
+    str_index = f"{ii}".rjust(5, "0")
+    if save_outputs == True:
+        # Save training data
+        np.savez(os.path.join(data_dir, f"/sim_{str_index}"), 
+                fluxes=train_kernel.gp.fluxes,
+                xdata=train_kernel.xdata, ydata=train_kernel.ydata,
+                freq=train_kernel.freq, power=train_kernel.power,
+                ypred=ypred, power_pred=power_pred)
+
+    if save_plots == True:
+        # Save figure
+        if (ii % 10) == 0:
+            fig = train_kernel.plot_kernel_fit(ypred, text_blocks=[kf.format_title(par) for par in plist])
+            fig.savefig(os.path.join(plot_dir, f"/sim_{str_index}.png"), bbox_inches="tight")
+    
+    summary = {"sim_id": ii, "ymse": ymse, "pmse": pmse}
+
+    return {**summary, **hyperparam, **pdict}
+
+
+# ================================================
 
 if __name__ == "__main__":
-
-    for path in ["../results/plots/", "../results/data/"]:
-        if not os.path.exists(path):
-            os.makedirs(path)
 
     index_array = np.arange(len(train_dict))
 
@@ -69,4 +90,4 @@ if __name__ == "__main__":
             
     # save results to csv file 
     result_df = pd.DataFrame(result_dict)
-    result_df.to_csv("../files/train_parameters_fit_coefficients.csv", index=False)
+    result_df.to_csv(save_file, index=False)
